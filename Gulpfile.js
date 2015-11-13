3fd94041f98a533b95ba82450536ad8afa584da1
m-vdb <mvergerdelbove@work4labs.com>
2015-11-13 07:55:49 -0800

    gulp compass uses bourbon path

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,16 @@
-var gulp = require('gulp'),
+var path = require('path'),
+    gulp = require('gulp'),
     compass = require('gulp-compass');
+
+var bourbonEntryPoint = require.resolve('bourbon'),
+    bourbonDir = path.dirname(bourbonEntryPoint) + '/app/assets/stylesheets';
+
  
 gulp.task('compass', function() {
   gulp.src('./sass/*.scss')
     .pipe(compass({
       config_file: './config.rb',
+      import_path: [bourbonDir],
       css: 'css',
       sass: 'sass'
     }))
