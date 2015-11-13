84c70bd2e5ce9813945a1f6e023c3a3c2da132b2
m-vdb <mvergerdelbove@work4labs.com>
2015-11-13 08:19:24 -0800

    new task: gulp compass:watch

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,16 +3,30 @@ var path = require('path'),
     compass = require('gulp-compass');
 
 var bourbonEntryPoint = require.resolve('bourbon'),
-    bourbonDir = path.dirname(bourbonEntryPoint) + '/app/assets/stylesheets';
-
+    bourbonDir = path.dirname(bourbonEntryPoint) + '/app/assets/stylesheets',
+    sassDir = './sass/*.scss',
+    sassConfig = './config.rb',
+    cssDir = 'css';
  
 gulp.task('compass', function() {
-  gulp.src('./sass/*.scss')
+  gulp.src(sassDir)
     .pipe(compass({
-      config_file: './config.rb',
+      config_file: sassConfig,
       import_path: [bourbonDir],
       css: 'css',
       sass: 'sass'
     }))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest(cssDir));
+});
+
+gulp.task('compass:watch', function() {
+  gulp.src(sassDir)
+    .pipe(compass({
+      config_file: sassConfig,
+      import_path: [bourbonDir],
+      css: 'css',
+      sass: 'sass',
+      task: 'watch'
+    }))
+    .pipe(gulp.dest(cssDir));
 });
