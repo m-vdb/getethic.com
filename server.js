a08674496f22adff29816f8edce7f76a0cc6c864
m-vdb <mvergerdelbove@work4labs.com>
2015-12-01 09:08:55 -0800

    listen on 0.0.0.0 in server.js

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ http.createServer(function (request, response) {
         // serve files
         file.serve(request, response);
     }).resume();
-}).listen(PORT, function(){
+}).listen(PORT, '0.0.0.0', function(){
     console.log("Server listening on: http://localhost:%s", PORT);
 });
