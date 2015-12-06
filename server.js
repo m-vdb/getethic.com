b454bb4530e46deecd479cd3cd7ca3967c763b74
m-vdb <mvergerdelbove@work4labs.com>
2015-12-05 20:22:51 -0800

    setup the /register-beta route

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-var express = require('express');
+var express = require('express'),
+    form = require('express-form'),
+    bodyParser = require('body-parser'),
+    field = form.field;
+
 const PORT = 8080;
 
 var app = express();
 
-// configure view folders
+// configuration
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({extended: false}));
 
 // serve static files
 app.use(express.static('public'));
@@ -24,7 +29,18 @@ app.get('/how-to-get-started', function (req, res) {
 app.get('/thanks', function (req, res) {
   res.render('thanks.html');
 });
-
+app.post(
+  '/register-beta',
+  form(field("email").trim().isEmail()),
+  function (req, res) {
+  if (!req.form.isValid) {
+    res.redirect('/#error');
+  } else {
+    console.log("Email:", req.form.email);
+    // TODO save in database
+    res.redirect('/thanks');
+  }
+});
 
 app.listen(PORT, '0.0.0.0', function () {
   console.log("Server listening on: http://localhost:%s", PORT);
