8b7f885fcce7c59621517d854bfc30361835ac1a
m-vdb <mvergerdelbove@work4labs.com>
2015-12-07 19:25:03 -0800

    close expanded FAQ items when opening a new one

--- public/js/Ethic/faq.js
+++ public/js/Ethic/faq.js
@@ -4,9 +4,17 @@ var UI = require('./UI.js');
 module.exports = {
   init: function () {
     if (UI.$faq.length) {
-      UI.$faq.find('span').on('click', function(e) {
+      UI.$faq.find('.js-faq-item-link').on('click', function(e) {
         e.preventDefault();
+        var expandedOnes = UI.$faq.filter('.expanded');
+
+        // open the right one
         $(this).closest(UI.$faq).toggleClass('expanded');
+
+        // close everything that we opened
+        expandedOnes.each(function () {
+          $(this).closest(UI.$faq).toggleClass('expanded');
+        });
       });
     }
   }
