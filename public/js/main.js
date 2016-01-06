e55ee34ce293026a8d5c5f596ca9842679319a15
zmjoret <mjoret@zeeagency.com>
2016-01-06 17:15:37 +0100

    optim fastclick mobile

--- public/js/main.js
+++ public/js/main.js
@@ -8,8 +8,10 @@ var UI = require('./Ethic/UI.js');
 var nav = require('./Ethic/nav.js');
 var register = require('./Ethic/register.js');
 var slider = require('./Ethic/slider.js');
+var click = require('./Ethic/click.js');
 
 UI.init();
 nav.init();
 register.init();
 slider.init();
+click.init();
