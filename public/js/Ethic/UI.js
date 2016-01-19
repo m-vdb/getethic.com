d5f11ccd7202961af58e4defbdde967c222db96d
m-vdb <mvergerdelbove@work4labs.com>
2016-01-18 22:52:49 -0800

    click on the whole slider should trigger a slide

--- public/js/Ethic/UI.js
+++ public/js/Ethic/UI.js
@@ -18,7 +18,7 @@ var UI = {
 		UI.$video = 				$('video');
 		UI.$nav = 					$('.menu');
 		UI.$page = 					$('.page');
-		UI.$slider = 				$('.claims');
+		UI.$claimsSlider = 	$('.claims');
 		UI.$faq = 					$('.question');
 
 
