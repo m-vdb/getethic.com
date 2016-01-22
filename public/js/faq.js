f1cf185825bf20ebe1dfefd965fe62ac7232bd84
m-vdb <mvergerdelbove@work4labs.com>
2016-01-21 22:02:03 -0800

    add fastclick to faq.js

--- public/js/faq.js
+++ public/js/faq.js
@@ -7,9 +7,11 @@ require('./Ethic/scroll.js');
 var UI = require('./Ethic/UI.js');
 var nav = require('./Ethic/nav.js');
 var faq = require('./Ethic/faq.js');
+var click = require('./Ethic/click.js');
 
 $("document").ready(function() {
   UI.init();
   nav.init();
   faq.init();
+  click.init();
 });
