9e9a0e4e62580dfa02aa441ffc21d9e5fcfa26f0
m-vdb <mvergerdelbove@work4labs.com>
2016-02-02 21:19:51 -0800

    custom "static" template tag

--- server.js
+++ server.js
@@ -27,6 +27,24 @@ app.use(cookieParser());
 
 // serve static files
 app.use(express.static('public'));
+// custom template tag
+swig.setTag('static', function (str, line, parser, types, options) {
+  var matched;  // only one token
+  parser.on("*", function (token) {
+    if (!matched &&
+        (token.type === types.STRING)
+      ) {
+      this.out.push(token.match.substring(1, token.length - 1));  // remove the ""
+      this.out.push('?v=' + config.get('cache_version'));
+      matched = true;
+      return;
+    }
+    throw new Error('Unexpected token "' + token.match + '" in static tag');
+  });
+  return true;
+}, function (compiler, args, content, parents, options, blockName) {
+  return '_output += "'+ args.join('') + '";';
+});
 
 // template context
 app.use(function (req, res, next) {
